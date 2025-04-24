890bc6e76fb609c4420607b1b97c22cbb2ea4510
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-04-24 22:16:46 +0500

    update

---
+++ HeroMotion/android/app/src/main/kotlin/com/example/heromotion/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.heromotion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ HeroMotion/lib/product_class.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class Producds{
+
+  final title;
+  final  subtitle;
+  final images:
+   products({required this.title, required this.b})
+}
