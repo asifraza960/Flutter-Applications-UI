1cc609ceee49424550b405cd96e5d915f77d206d
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-04-25 23:11:44 +0500

    update

--- HeroMotion/lib/product_class.dart
+++ HeroMotion/lib/product_class.dart
@@ -1,11 +1,16 @@
-import 'package:flutter/cupertino.dart';
+class Products {
+  String title;
+  double price;
+  String imagespath;
 
-class Producds{
-
-  String  title;
- double   price;
- String   imagespath;
-
-  Producds({required this.title, required this.price, required this.imagespath});
+  Products({required this.title, required this.price, required this.imagespath});
 
 }
+List<Products> productlis =[
+  Products(title: 'Burger', price: 33.0, imagespath: "asset/images/burger.jpg"
+  ),
+  Products(title: 'Pizza', price: 2300.0, imagespath: "asset/images/burger.jpg"
+  ),
+  Products(title: 'Zinger', price: 500.0, imagespath: "asset/images/burger.jpg"
+  )
+];
