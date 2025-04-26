002f7b367c713a7b88dc8c62a35cdfdcf9522380
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-04-26 09:03:14 +0500

    update

--- HeroMotion/lib/product_class.dart
+++ HeroMotion/lib/product_class.dart
@@ -6,11 +6,24 @@ class Products {
   Products({required this.title, required this.price, required this.imagespath});
 
 }
+
+
+
+
 List<Products> productlis =[
-  Products(title: 'Burger', price: 33.0, imagespath: "asset/images/burger.jpg"
+  Products(title: 'Burger', price: 550.0, imagespath: "asset/images/burger.jpg"
   ),
-  Products(title: 'Pizza', price: 2300.0, imagespath: "asset/images/burger.jpg"
+  Products(title: 'Pizza', price: 2300.0, imagespath: "asset/images/pizza.jpg"
   ),
-  Products(title: 'Zinger', price: 500.0, imagespath: "asset/images/burger.jpg"
-  )
+  Products(title: 'Zinger', price: 500.0, imagespath: "asset/images/wings.jpg"
+  ),
+Products(title: 'chips', price: 300.0, imagespath: "asset/images/chip.jpg"),
+  Products(title: 'Burger', price: 550.0, imagespath: "asset/images/burger.jpg"
+  ),
+  Products(title: 'Pizza', price: 2300.0, imagespath: "asset/images/pizza.jpg"
+  ),
+  Products(title: 'Zinger', price: 500.0, imagespath: "asset/images/wings.jpg"
+  ),
+  Products(title: 'chips', price: 300.0, imagespath: "asset/images/chip.jpg")
+
 ];
