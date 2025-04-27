eae9debc8e8d2136f59d3b539f2ad420a92faf99
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-04-27 23:21:42 +0500

    update

--- HeroMotion/lib/productDetailpage.dart
+++ HeroMotion/lib/productDetailpage.dart
@@ -2,48 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:heromotion/product_class.dart';
 
 class Productdetailpage extends StatelessWidget {
-  const Productdetailpage({super.key,});
+  const Productdetailpage({super.key});
   @override
   Widget build(BuildContext context) {
     var products = ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
-      appBar:AppBar(
-        title:Text(products.title),
-      ),
+      appBar: AppBar(title: Text(products.title)),
       body: Center(
         child: Column(
           children: [
             Expanded(
               flex: 3,
-                child: Stack(
-                  children: [
-                    Container(
-                      height: double.infinity,
-                      width: double.infinity,
-                      color: Colors.brown,
-                      child: Hero(
-                        tag: products.title,
-                        placeholderBuilder: (context, heroSize, child) => Opacity(opacity:0.5,child: child,),
-                        child: CircleAvatar(
-                          backgroundImage: AssetImage(products.imagespath),
-                          radius: 100,
-                        ),
-
-
+              child: Stack(
+                children: [
+                  Container(
+                    height: double.infinity,
+                    width: double.infinity,
+                    color: Colors.brown,
+                    child: Hero(
+                      tag: products.title,
+                      placeholderBuilder:
+                          (context, heroSize, child) =>
+                              Opacity(opacity: 0.5, child: child),
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage(products.imagespath),
+                        radius: 100,
                       ),
                     ),
-                    
-                  ],
-                )
+                  ),
+                ],
+              ),
             ),
             Expanded(
               flex: 7,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("this is an amazing")
-                  ],
-                ))
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text("this is an amazing")],
+              ),
+            ),
           ],
         ),
       ),
--- HeroMotion/lib/productDetails.dart
+++ HeroMotion/lib/productDetails.dart
@@ -5,33 +5,44 @@ import 'package:heromotion/product_class.dart';
 class ProductListTile extends StatelessWidget {
   const ProductListTile({super.key, required this.product});
   final Product product;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: Hero(
         tag: product.title,
-        child: CircleAvatar(backgroundImage: AssetImage(product.imagespath)),
-        placeholderBuilder: (context, heroSize, child) => CircleAvatar(
-          backgroundColor: Colors.grey,
+        child: CircleAvatar(
+          backgroundImage: AssetImage(product.imagespath),
         ),
-        flightShuttleBuilder: (flightContext, animation, flightDirection, fromHeroContext, toHeroContext) => RotationTransition(turns: animation.drive(Tween(begin: 0.0,end: 3.0),
-        ),
-          child: ScaleTransition(scale: animation.drive(Tween(begin: 0.0,end: 5.0)),
-            child: flightContext.widget,
-          ),
-
-        ),
+        flightShuttleBuilder: (
+            flightContext,
+            animation,
+            flightDirection,
+            fromHeroContext,
+            toHeroContext,
+            ) {
+          return RotationTransition(
+            turns: animation.drive(Tween(begin: 0.0, end: 5.0)),
+            child: ScaleTransition(
+              scale: animation.drive(Tween(begin: 1.0, end: 3.0)),
+              child: (flightDirection == HeroFlightDirection.push
+                  ? fromHeroContext.widget
+                  : toHeroContext.widget),
+            ),
+          );
+        },
       ),
       title: Text(product.title),
       subtitle: Text(product.price.toString()),
       onTap: () {
         Navigator.of(context).push(
           PageRouteBuilder(
-            pageBuilder:
-                (context, animation, secondaryAnimation) => Productdetailpage(),
+            pageBuilder: (context, animation, secondaryAnimation) {
+              return Productdetailpage();
+            },
             settings: RouteSettings(arguments: product),
-            transitionDuration: Duration(seconds: 1),
-            reverseTransitionDuration: Duration(seconds: 1)
+            transitionDuration: const Duration(seconds: 1),
+            reverseTransitionDuration: const Duration(seconds: 1),
           ),
         );
       },
